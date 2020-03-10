ad372afa24a61805a621f33ae6c8d493e4fb84b9
ehsan <ehsan@peymans.com>
2020-03-11 02:04:21 +0330

    TODOs done

--- hw1/cs285/infrastructure/replay_buffer.py
+++ hw1/cs285/infrastructure/replay_buffer.py
@@ -67,7 +67,8 @@ class ReplayBuffer(object):
         ## HINT 1: use np.random.permutation to sample random indices
         ## HINT 2: return corresponding data points from each array (i.e., not different indices from each array)
         ## HINT 3: look at the sample_recent_data function below
-        return TODO, TODO, TODO, TODO, TODO
+        random_indices = np.random.permutation(batch_size)
+        return self.obs[random_indices],self.acs[random_indices],self.rews[random_indices],self.next_obs[random_indices]
 
     def sample_recent_data(self, batch_size=1):
         return self.obs[-batch_size:], self.acs[-batch_size:], self.rews[-batch_size:], self.next_obs[-batch_size:], self.terminals[-batch_size:]
--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -87,8 +87,8 @@ class MLPPolicy(BasePolicy):
         # HINT1: you will need to call self.sess.run
         # HINT2: the tensor we're interested in evaluating is self.sample_ac
         # HINT3: in order to run self.sample_ac, it will need observation fed into the feed_dict
-        return TODO
-
+        return self.sess.run(self.sample_ac,feed_dict={self.observations_pl:observation})[0]
+        # ?? what are other indexes of session.run
     # update/train this policy
     def update(self, observations, actions):
         raise NotImplementedError
@@ -121,10 +121,10 @@ class MLPPolicySL(MLPPolicy):
         # TODO define the loss that will be used to train this policy
         # HINT1: remember that we are doing supervised learning
         # HINT2: use tf.losses.mean_squared_error
-        self.loss = TODO
+        self.loss = tf.losses.mean_squared_error(true_actions,predicted_actions)
         self.train_op = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
     def update(self, observations, actions):
         assert(self.training, 'Policy must be created with training=True in order to perform training updates...')
-        self.sess.run(self.train_op, feed_dict={self.observations_pl: observations, self.acs_labels_na: actions})
-
+        _,loss = self.sess.run(self.train_op, feed_dict={self.observations_pl: observations, self.acs_labels_na: actions})
+        # ?? what is the value of _
