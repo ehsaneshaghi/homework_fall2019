b6064b0969fd4bef4f7882166ba93fc075938e37
ehsaneshaghi <ehsan.eshaghi94@gmail.com>
2020-05-26 15:05:29 +0430

    ???

--- hw3/cs285/agents/dqn_agent.py
+++ hw3/cs285/agents/dqn_agent.py
@@ -49,17 +49,17 @@ class DQNAgent(object):
 
         # TODO store the latest observation into the replay buffer
         # HINT: see replay buffer's function store_frame
-        self.replay_buffer_idx = TODO
+        self.replay_buffer_idx = self.replay_buffer.store_frame(self.last_obs)#TODO
 
         eps = self.exploration.value(self.t)
         # TODO use epsilon greedy exploration when selecting action
         # HINT: take random action 
             # with probability eps (see np.random.random())
             # OR if your current step number (see self.t) is less that self.learning_starts
-        perform_random_action = TODO
+        perform_random_action = np.random.random() < eps or self.t < self.learning_starts#TODO
 
         if perform_random_action:
-            action = TODO
+            action = np.random.randint(self.num_actions)#TODO
         else:
             # TODO query the policy to select action
             # HINT: you cannot use "self.last_obs" directly as input
@@ -70,26 +70,27 @@ class DQNAgent(object):
             # that you pushed into the buffer and compute the corresponding
             # input that should be given to a Q network by appending some
             # previous frames.
-            enc_last_obs = TODO
+            enc_last_obs = self.replay_buffer.encode_recent_observation()#TODO
             enc_last_obs = enc_last_obs[None, :]
 
             # TODO query the policy with enc_last_obs to select action
-            action = TODO
+            action = self.actor.get_action(enc_last_obs)#TODO
             action = action[0]
 
         # TODO take a step in the environment using the action from the policy
         # HINT1: remember that self.last_obs must always point to the newest/latest observation
         # HINT2: remember the following useful function that you've seen before:
             #obs, reward, done, info = env.step(action)
-        TODO
+        obs,reward,done,info = self.env.step(action) #TODO
 
         # TODO store the result of taking this action into the replay buffer
         # HINT1: see replay buffer's store_effect function
         # HINT2: one of the arguments you'll need to pass in is self.replay_buffer_idx from above
-        TODO
+        self.replay_buffer.store_effect(self.replay_buffer_idx,action,reward,done)#TODO
 
         # TODO if taking this step resulted in done, reset the env (and the latest observation)
-        TODO
+        if done: #TODO
+            self.last_obs = self.env.reset()
 
     def sample(self, batch_size):
         if self.replay_buffer.can_sample(self.batch_size):
@@ -111,18 +112,19 @@ class DQNAgent(object):
 
             # TODO populate all placeholders necessary for calculating the critic's total_error
             # HINT: obs_t_ph, act_t_ph, rew_t_ph, obs_tp1_ph, done_mask_ph
+            obs_t_ph, act_t_ph, rew_t_ph, obs_tp1_ph, done_mask_ph = self.sample(self.batch_size) #TODO
             feed_dict = {
                 self.critic.learning_rate: self.optimizer_spec.lr_schedule.value(self.t),
-                TODO,
-                TODO,
-                TODO,
-                TODO,
-                TODO,
+                self.critic.obs_t_ph:obs_t_ph, #TODO
+                self.critic.act_h_ph:act_t_ph, #TODO
+                self.critic.rew_t_ph:rew_t_ph, #TODO
+                self.critic.obs_tp1_ph = obs_tp1_ph, #TODO
+                self.critic.done_mask_ph = done_mask_ph, #TODO
             }
 
             # TODO: create a LIST of tensors to run in order to 
             # train the critic as well as get the resulting total_error
-            tensors_to_run = TODO
+            tensors_to_run = #TODO
             loss, _ = self.sess.run(tensors_to_run, feed_dict=feed_dict)
             # Note: remember that the critic's total_error value is what you
             # created to compute the Bellman error in a batch, 
@@ -138,3 +140,4 @@ class DQNAgent(object):
 
         self.t += 1
         return loss
+
